aa52b8923452705e999a9b1b5fb5e078ec85a679
Blake Peck <s210561@students.aie.edu.au>
2021-03-09 16:07:02 +1100

    Replace the code with the Vector 3 and Vector 4 files in the Unit Test, with the code from the Math Library Vector 3 and Vector 4 code files

--- MathClasses/Vector3.cs
+++ MathClasses/Vector3.cs
@@ -6,16 +6,83 @@ using System.Threading.Tasks;
 
 namespace MathClasses
 {
-    public class Vector3
-    {
-        public float x, y, z;
+	public struct Vector3
+	{
+		public float x;
+		public float y;
+		public float z;
 
-        public Vector3()
-        {
-            x = 0;
-            y = 0;
-            z = 0;
-        }
+		//Constructors
+		public Vector3(float x = 0.0f, float y = 0.0f, float z = 0.0f)
+		{
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
 
-    }
+		//Operator overloading
+		//V = V + V (point translated by a vector)
+		public static Vector3 operator + (Vector3 lhs, Vector3 rhs)
+		{
+			Vector3 result;
+			result.x = lhs.x + rhs.x;
+			result.y = lhs.y + rhs.y;
+			result.z = lhs.z + rhs.z;
+
+			return result;
+		}
+
+		//V = V – V(point translated by a vector)
+		public static Vector3 operator - (Vector3 lhs, Vector3 rhs)
+		{
+			Vector3 result;
+			result.x = lhs.x - rhs.x;
+			result.y = lhs.y - rhs.y;
+			result.z = lhs.z - rhs.z;
+
+			return result;
+		}
+
+		//V = V x f (vector scale)
+		public static Vector3 operator * (Vector3 lhs, float rhs)
+		{
+			Vector3 result;
+			result.x = lhs.x * rhs;
+			result.y = lhs.y * rhs;
+			result.z = lhs.z * rhs;
+
+			return result;
+		}
+
+		//V = f x V (vector scale)
+		public static Vector3 operator * (float lhs, Vector3 rhs)
+		{
+			Vector3 result;
+			result.x = lhs * rhs.x;
+			result.y = lhs * rhs.y;
+			result.z = lhs * rhs.z;
+
+			return result;
+		}
+
+		//f = V.Magnitude()
+		public float Magnitude()
+		{
+			//c^2 = sqrt(a^2 + b^2)
+			return (float)Math.Sqrt((x * x) + (y * y) + (z * z));
+		}
+
+		//Normalise()
+		public void Normalise()
+		{
+			float magnitude = Magnitude();
+			if (magnitude != 0)
+			{
+				x /= magnitude;
+				y /= magnitude;
+				z /= magnitude;
+			}
+
+		}
+	}
 }
--- MathClasses/Vector4.cs
+++ MathClasses/Vector4.cs
@@ -6,17 +6,90 @@ using System.Threading.Tasks;
 
 namespace MathClasses
 {
-    public class Vector4
-    {
-        public float x, y, z, w;
+	public struct Vector4
+	{
+		public float x;
+		public float y;
+		public float z;
+		public float w;
 
-        public Vector4()
-        {
-            x = 0;
-            y = 0;
-            z = 0;
-            w = 0;
-        }
+		//Constructors
+		public Vector4(float x = 0.0f, float y = 0.0f, float z = 0.0f, float w = 0.0f)
+		{
+			this.x = x;
+			this.y = y;
+			this.z = z;
+			this.w = w;
+		}
 
-    }
+		//Operator overloading
+		//V = V + V (point translated by a vector)
+		public static Vector4 operator + (Vector4 lhs, Vector4 rhs)
+		{
+			Vector4 result;
+			result.x = lhs.x + rhs.x;
+			result.y = lhs.y + rhs.y;
+			result.z = lhs.z + rhs.z;
+			result.w = lhs.w + rhs.w;
+
+			return result;
+		}
+
+		//V = V – V(point translated by a vector)
+		public static Vector4 operator - (Vector4 lhs, Vector4 rhs)
+		{
+			Vector4 result;
+			result.x = lhs.x - rhs.x;
+			result.y = lhs.y - rhs.y;
+			result.z = lhs.z - rhs.z;
+			result.w = lhs.w - rhs.w;
+
+			return result;
+		}
+
+		//V = V x f (vector scale)
+		public static Vector4 operator * (Vector4 lhs, float rhs)
+		{
+			Vector4 result;
+			result.x = lhs.x * rhs;
+			result.y = lhs.y * rhs;
+			result.z = lhs.z * rhs;
+			result.w = lhs.w * rhs;
+
+			return result;
+		}
+
+
+		public static Vector4 operator * (float lhs, Vector4 rhs)
+		{
+			Vector4 result;
+			result.x = lhs * rhs.x;
+			result.y = lhs * rhs.y;
+			result.z = lhs * rhs.z;
+			result.w = lhs * rhs.w;
+
+			return result;
+		}
+
+		//f = V.Magnitude()
+		public float Magnitude()
+		{
+			//c^2 = sqrt(a^2 + b^2)
+			return (float)Math.Sqrt((x * x) + (y * y) + (z * z) + (w * w));
+		}
+
+		//Normalise()
+		public void Normalise()
+		{
+			float magnitude = Magnitude();
+			if (magnitude != 0)
+			{
+				x /= magnitude;
+				y /= magnitude;
+				z /= magnitude;
+				w /= magnitude;
+			}
+
+		}
+	}
 }
