a71a326221a960b1fa256b10a8abd2ea6ba04f9a
Blake Peck <s210561@students.aie.edu.au>
2021-03-10 10:30:08 +1100

    Grabbed the code of Vector 3 and 4 script from MathLibrary.sln as paste it over to UnitTest.sln into the other Vector 3 and 4 script as I had forgot about this file which is where the coding should be done on now

--- MathClasses/Vector3.cs
+++ MathClasses/Vector3.cs
@@ -84,5 +84,22 @@ namespace MathClasses
 			}
 
 		}
+
+		//f = V.Dot( V )
+		public float Dot(Vector3 rhs)
+		{
+			return (x * rhs.x) + (y * rhs.y) + (z * rhs.z);
+		}
+
+		//V = V.Cross( V )
+		public Vector3 Cross(Vector3 rhs)
+		{
+			Vector3 result;
+			result.x = (y * rhs.z) - (z * rhs.y);
+			result.y = (z * rhs.x) - (x * rhs.z);
+			result.z = (x * rhs.y) - (y * rhs.x);
+
+			return result;
+		}
 	}
 }
