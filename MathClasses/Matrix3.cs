96dd0bdb0e0f61241184ea79de17c7b328c7fc1a
Blake Peck <s210561@students.aie.edu.au>
2021-03-15 14:36:45 +1100

    copied over the code to UnitTest

--- MathClasses/Matrix3.cs
+++ MathClasses/Matrix3.cs
@@ -71,5 +71,29 @@ namespace MathClasses
 
 			return result;
 		}
+
+		public void SetRotateX(float fRadians)
+		{
+			m[4] = (float)Math.Cos(fRadians);
+			m[5] = (float)-Math.Sin(fRadians);
+			m[7] = (float)Math.Sin(fRadians);
+			m[8] = (float)Math.Cos(fRadians);
+		}
+
+		public void SetRotateY(float fRadians)
+		{
+			m[0] = (float)Math.Cos(fRadians);
+			m[2] = (float)Math.Sin(fRadians);
+			m[6] = (float)-Math.Sin(fRadians);
+			m[8] = (float)Math.Cos(fRadians);
+		}
+
+		public void SetRotateZ(float fRadians)
+		{
+			m[0] = (float)Math.Cos(fRadians);
+			m[1] = (float)-Math.Sin(fRadians);
+			m[3] = (float)Math.Sin(fRadians);
+			m[4] = (float)Math.Cos(fRadians);
+		}
 	}
 }
