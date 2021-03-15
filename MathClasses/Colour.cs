7604c53088b02796ece80ced2dd51d3df4a6819c
Blake Peck <s210561@students.aie.edu.au>
2021-03-15 12:31:18 +1100

    Copied the code into the UnitTest visual studio

--- MathClasses/Colour.cs
+++ MathClasses/Colour.cs
@@ -6,14 +6,64 @@ using System.Threading.Tasks;
 
 namespace MathClasses
 {
-    public class Colour
-    {
-        public UInt32 colour;
+	class Colour
+	{
+		private uint colour = 0;
 
-        public Colour()
-        {
-            colour = 0;
-        }
+		public Colour(byte red, byte green, byte blue, byte alpha)
+		{
+			//0x00000000
+			//00 00 00 00
+			//r  g  b  a
+			colour = (uint)((red << 24) + (green << 16) + (blue << 8) + alpha);
+		}
 
-    }
+		//Red
+		public void SetRed(byte red)
+		{
+			colour = colour & 0xFF000000;
+			colour = colour | (uint)(red << 24);
+		}
+
+		public byte GetRed()
+		{
+			return (byte)(colour >> 24);
+		}
+
+		//Green
+		public void SetGreen(byte green)
+		{
+			colour = colour & 0x00FF0000;
+			colour = colour | (uint)(green << 16);
+		}
+
+		public byte GetGreen()
+		{
+			return (byte)((colour << 8) >> 24);
+		}
+
+		//Blue
+		public void SetBlue(byte blue)
+		{
+			colour = colour & 0x0000FF00;
+			colour = colour | (uint)(blue << 24);
+		}
+
+		public byte GetBlue()
+		{
+			return (byte)(colour >> 24);
+		}
+
+		//Alpha
+		public void SetAlpha(byte Alpha)
+		{
+			colour = colour & 0x000000FF;
+			colour = colour | (uint)(Alpha);
+		}
+
+		public byte GetAlpha()
+		{
+			return (byte)((colour << 24) >> 24);
+		}
+	}
 }
--- MathClasses/Matrix3.cs
+++ MathClasses/Matrix3.cs
@@ -6,16 +6,70 @@ using System.Threading.Tasks;
 
 namespace MathClasses
 {
-    public class Matrix3
-    {
-        public float m1, m2, m3, m4, m5, m6, m7, m8, m9;
+	struct Matrix3
+	{
+		public float[] m;
 
-        public Matrix3()
-        {
-            m1 = 1; m2 = 0; m3 = 0;
-            m4 = 0; m5 = 1; m6 = 0;
-            m7 = 0; m8 = 0; m9 = 1;
-        }
+		public Matrix3(bool bDefault = true)
+		{
+			m = new float[9];
+			m[0] = 1;
+			m[1] = 0;
+			m[2] = 0;
+			m[3] = 0;
+			m[4] = 1;
+			m[5] = 0;
+			m[6] = 0;
+			m[7] = 0;
+			m[8] = 1;
+		}
 
-    }
+		public Matrix3(float m0, float m1, float m2,
+						float m3, float m4, float m5,
+						float m6, float m7, float m8)
+		{
+			m = new float[9];
+			m[0] = m0;
+			m[1] = m1;
+			m[2] = m2;
+			m[3] = m3;
+			m[4] = m4;
+			m[5] = m5;
+			m[6] = m6;
+			m[7] = m7;
+			m[8] = m8;
+		}
+
+		//V = M x V (vector transformation)
+		public static Vector3 operator *(Matrix3 lhs, Vector3 rhs)
+		{
+			Vector3 result;
+
+			result.x = (lhs.m[0] * rhs.x) + (lhs.m[3] * rhs.y) + (lhs.m[6] * rhs.z);
+			result.y = (lhs.m[1] * rhs.x) + (lhs.m[4] * rhs.y) + (lhs.m[7] * rhs.z);
+			result.z = (lhs.m[2] * rhs.x) + (lhs.m[5] * rhs.y) + (lhs.m[8] * rhs.z);
+
+			return result;
+		}
+
+		//M = M x M (matrix concatenation)
+		public static Matrix3 operator *(Matrix3 lhs, Matrix3 rhs)
+		{
+			Matrix3 result = new Matrix3();
+
+			result.m[0] = (lhs.m[0] * rhs.m[0]) + (lhs.m[3] * rhs.m[1]) + (lhs.m[6] * rhs.m[2]);
+			result.m[1] = (lhs.m[1] * rhs.m[0]) + (lhs.m[4] * rhs.m[1]) + (lhs.m[7] * rhs.m[2]);
+			result.m[2] = (lhs.m[2] * rhs.m[0]) + (lhs.m[5] * rhs.m[1]) + (lhs.m[8] * rhs.m[2]);
+
+			result.m[3] = (lhs.m[0] * rhs.m[3]) + (lhs.m[3] * rhs.m[4]) + (lhs.m[6] * rhs.m[5]);
+			result.m[4] = (lhs.m[1] * rhs.m[3]) + (lhs.m[4] * rhs.m[4]) + (lhs.m[7] * rhs.m[5]);
+			result.m[5] = (lhs.m[2] * rhs.m[3]) + (lhs.m[5] * rhs.m[4]) + (lhs.m[8] * rhs.m[5]);
+
+			result.m[6] = (lhs.m[0] * rhs.m[6]) + (lhs.m[3] * rhs.m[7]) + (lhs.m[6] * rhs.m[8]);
+			result.m[7] = (lhs.m[1] * rhs.m[6]) + (lhs.m[4] * rhs.m[7]) + (lhs.m[7] * rhs.m[8]);
+			result.m[8] = (lhs.m[2] * rhs.m[6]) + (lhs.m[5] * rhs.m[7]) + (lhs.m[8] * rhs.m[8]);
+
+			return result;
+		}
+	}
 }
