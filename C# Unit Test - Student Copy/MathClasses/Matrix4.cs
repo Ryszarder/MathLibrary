323faf8de2d3bf083227038d8d4f2d5a4a04c80c
Blake Peck <s210561@students.aie.edu.au>
2021-03-22 10:22:21 +1100

    change the array to floats as it work better with the unit test

--- C# Unit Test - Student Copy/MathClasses/Matrix4.cs
+++ C# Unit Test - Student Copy/MathClasses/Matrix4.cs
@@ -10,7 +10,7 @@ namespace MathClasses
     {
         public float m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16;
 
-        public Matrix4()
+        public Matrix4(bool bDefault = true)
         {
             m1 = 1; m2 = 0; m3 = 0; m4 = 0;
             m5 = 0; m6 = 1; m7 = 0; m8 = 0;
@@ -18,15 +18,38 @@ namespace MathClasses
             m13= 0; m14= 0; m15= 0; m16= 1;
         }
 
+		public Matrix4(float m1, float m2, float m3, float m4,
+						float m5, float m6,float m7, float m8,
+						float m9,float m10, float m11, float m12,
+						float m13, float m14, float m15,float m16)
+		{
+			this.m1 = m1;
+			this.m2 = m2;
+			this.m3 = m3;
+			this.m4 = m4;
+			this.m5 = m5;
+			this.m6 = m6;
+			this.m7 = m7;
+			this.m8 = m8;
+			this.m9 = m9;
+			this.m10 = m10;
+			this.m11 = m11;
+			this.m12 = m12;
+			this.m13 = m13;
+			this.m14 = m14;
+			this.m15 = m15;
+			this.m16 = m16;
+		}
+
 		//V = M x V (vector transformation)
 		public static Vector4 operator *(Matrix4 lhs, Vector4 rhs)
 		{
 			Vector4 result;
 
-			result.x = (lhs.m[0] * rhs.x) + (lhs.m[4] * rhs.y) + (lhs.m[8] * rhs.z) + (lhs.m[12] * rhs.w);
-			result.y = (lhs.m[1] * rhs.x) + (lhs.m[5] * rhs.y) + (lhs.m[9] * rhs.z) + (lhs.m[13] * rhs.w);
-			result.z = (lhs.m[2] * rhs.x) + (lhs.m[6] * rhs.y) + (lhs.m[10] * rhs.z) + (lhs.m[14] * rhs.w);
-			result.w = (lhs.m[3] * rhs.x) + (lhs.m[7] * rhs.y) + (lhs.m[11] * rhs.z) + (lhs.m[15] * rhs.w);
+			result.x = (lhs.m1 * rhs.x) + (lhs.m5 * rhs.y) + (lhs.m9 * rhs.z) + (lhs.m13 * rhs.w);
+			result.y = (lhs.m2 * rhs.x) + (lhs.m6 * rhs.y) + (lhs.m10 * rhs.z) + (lhs.m14 * rhs.w);
+			result.z = (lhs.m3 * rhs.x) + (lhs.m7 * rhs.y) + (lhs.m11 * rhs.z) + (lhs.m15 * rhs.w);
+			result.w = (lhs.m4 * rhs.x) + (lhs.m8 * rhs.y) + (lhs.m12 * rhs.z) + (lhs.m16 * rhs.w);
 
 			return result;
 		}
@@ -36,25 +59,25 @@ namespace MathClasses
 		{
 			Matrix4 result = new Matrix4();
 
-			result.m[0] = (lhs.m[0] * rhs.m[0]) + (lhs.m[4] * rhs.m[1]) + (lhs.m[8] * rhs.m[2]) + (lhs.m[12] * rhs.m[3]);
-			result.m[1] = (lhs.m[1] * rhs.m[0]) + (lhs.m[5] * rhs.m[1]) + (lhs.m[9] * rhs.m[2]) + (lhs.m[13] * rhs.m[3]);
-			result.m[2] = (lhs.m[2] * rhs.m[0]) + (lhs.m[6] * rhs.m[1]) + (lhs.m[10] * rhs.m[2]) + (lhs.m[14] * rhs.m[3]);
-			result.m[3] = (lhs.m[3] * rhs.m[0]) + (lhs.m[7] * rhs.m[1]) + (lhs.m[11] * rhs.m[2]) + (lhs.m[15] * rhs.m[3]);
+			result.m1 = (lhs.m1 * rhs.m1) + (lhs.m5 * rhs.m2) + (lhs.m9 * rhs.m3) +  (lhs.m13 * rhs.m4);
+			result.m2 = (lhs.m2 * rhs.m1) + (lhs.m6 * rhs.m2) + (lhs.m10 * rhs.m3) + (lhs.m14 * rhs.m4);
+			result.m3 = (lhs.m3 * rhs.m1) + (lhs.m7 * rhs.m2) + (lhs.m11 * rhs.m3) + (lhs.m15 * rhs.m4);
+			result.m4 = (lhs.m4 * rhs.m1) + (lhs.m8 * rhs.m2) + (lhs.m12 * rhs.m3) + (lhs.m16 * rhs.m4);
 
-			result.m[4] = (lhs.m[0] * rhs.m[4]) + (lhs.m[4] * rhs.m[5]) + (lhs.m[8] * rhs.m[6]) + (lhs.m[12] * rhs.m[7]);
-			result.m[5] = (lhs.m[1] * rhs.m[4]) + (lhs.m[5] * rhs.m[5]) + (lhs.m[9] * rhs.m[6]) + (lhs.m[13] * rhs.m[7]);
-			result.m[6] = (lhs.m[2] * rhs.m[4]) + (lhs.m[6] * rhs.m[5]) + (lhs.m[10] * rhs.m[6]) + (lhs.m[14] * rhs.m[7]);
-			result.m[7] = (lhs.m[3] * rhs.m[4]) + (lhs.m[7] * rhs.m[5]) + (lhs.m[11] * rhs.m[6]) + (lhs.m[15] * rhs.m[7]);
+			result.m5 = (lhs.m1 * rhs.m5) + (lhs.m5 * rhs.m6) + (lhs.m9 * rhs.m7) + (lhs.m13* rhs.m8);
+			result.m6 = (lhs.m2 * rhs.m5) + (lhs.m6 * rhs.m6) + (lhs.m10 * rhs.m7) + (lhs.m14 * rhs.m8);
+			result.m7 = (lhs.m3 * rhs.m5) + (lhs.m7 * rhs.m6) + (lhs.m11 * rhs.m7) + (lhs.m15 * rhs.m8);
+			result.m8 = (lhs.m4 * rhs.m5) + (lhs.m8 * rhs.m6) + (lhs.m12 * rhs.m7) + (lhs.m16 * rhs.m8);
 
-			result.m[8] = (lhs.m[0] * rhs.m[8]) + (lhs.m[4] * rhs.m[9]) + (lhs.m[8] * rhs.m[10]) + (lhs.m[12] * rhs.m[11]);
-			result.m[9] = (lhs.m[1] * rhs.m[8]) + (lhs.m[5] * rhs.m[9]) + (lhs.m[9] * rhs.m[10]) + (lhs.m[13] * rhs.m[11]);
-			result.m[10] = (lhs.m[2] * rhs.m[8]) + (lhs.m[6] * rhs.m[9]) + (lhs.m[10] * rhs.m[10]) + (lhs.m[14] * rhs.m[11]);
-			result.m[11] = (lhs.m[3] * rhs.m[8]) + (lhs.m[7] * rhs.m[9]) + (lhs.m[11] * rhs.m[10]) + (lhs.m[15] * rhs.m[11]);
+			result.m9 = (lhs.m1 * rhs.m9) + (lhs.m5 * rhs.m10) + (lhs.m9 * rhs.m11) + (lhs.m13* rhs.m12);
+			result.m10 = (lhs.m2 * rhs.m9) + (lhs.m6 * rhs.m10) + (lhs.m10 * rhs.m11) + (lhs.m14 * rhs.m12);
+			result.m11 = (lhs.m3 * rhs.m9) + (lhs.m7 * rhs.m10) + (lhs.m11 * rhs.m11) + (lhs.m15 * rhs.m12);
+			result.m12 = (lhs.m4 * rhs.m9) + (lhs.m8 * rhs.m10) + (lhs.m12 * rhs.m11) + (lhs.m16 * rhs.m12);
 
-			result.m[12] = (lhs.m[0] * rhs.m[12]) + (lhs.m[4] * rhs.m[13]) + (lhs.m[8] * rhs.m[14]) + (lhs.m[12] * rhs.m[15]);
-			result.m[13] = (lhs.m[1] * rhs.m[12]) + (lhs.m[5] * rhs.m[13]) + (lhs.m[9] * rhs.m[14]) + (lhs.m[13] * rhs.m[15]);
-			result.m[14] = (lhs.m[2] * rhs.m[12]) + (lhs.m[6] * rhs.m[13]) + (lhs.m[10] * rhs.m[14]) + (lhs.m[14] * rhs.m[15]);
-			result.m[15] = (lhs.m[3] * rhs.m[12]) + (lhs.m[7] * rhs.m[13]) + (lhs.m[11] * rhs.m[14]) + (lhs.m[15] * rhs.m[15]);
+			result.m13 = (lhs.m1 * rhs.m13) + (lhs.m5 * rhs.m14) + (lhs.m9 * rhs.m15) + (lhs.m13* rhs.m16);
+			result.m14 = (lhs.m2 * rhs.m13) + (lhs.m6 * rhs.m14) + (lhs.m10 * rhs.m15) + (lhs.m14 * rhs.m16);
+			result.m15 = (lhs.m3 * rhs.m13) + (lhs.m7 * rhs.m14) + (lhs.m11 * rhs.m15) + (lhs.m15 * rhs.m16);
+			result.m16 = (lhs.m4 * rhs.m13) + (lhs.m8 * rhs.m14) + (lhs.m12 * rhs.m15) + (lhs.m16 * rhs.m16);
 
 			return result;
 		}
@@ -62,59 +85,59 @@ namespace MathClasses
 		//SetRotateX( f )
 		public void SetRotateX(float fRadians)
 		{
-			m[5] = (float)Math.Cos(fRadians);
-			m[6] = (float)Math.Sin(fRadians);
-			m[9] = (float)-Math.Sin(fRadians);
-			m[10] = (float)Math.Cos(fRadians);
+			m6 = (float)Math.Cos(fRadians);
+			m7 = (float)Math.Sin(fRadians);
+			m10 = (float)-Math.Sin(fRadians);
+			m11 = (float)Math.Cos(fRadians);
 		}
 		//SetRotateY( f )
 		public void SetRotateY(float fRadians)
 		{
-			m[0] = (float)Math.Cos(fRadians);
-			m[2] = (float)-Math.Sin(fRadians);
-			m[8] = (float)Math.Sin(fRadians);
-			m[10] = (float)Math.Cos(fRadians);
+			m1 = (float)Math.Cos(fRadians);
+			m3 = (float)-Math.Sin(fRadians);
+			m9= (float)Math.Sin(fRadians);
+			m11 = (float)Math.Cos(fRadians);
 		}
 
 		//SetRotateZ( f )
 		public void SetRotateZ(float fRadians)
 		{
-			m[0] = (float)Math.Cos(fRadians);
-			m[1] = (float)Math.Sin(fRadians);
-			m[4] = (float)-Math.Sin(fRadians);
-			m[5] = (float)Math.Cos(fRadians);
+			m1 = (float)Math.Cos(fRadians);
+			m2 = (float)Math.Sin(fRadians);
+			m5 = (float)-Math.Sin(fRadians);
+			m6 = (float)Math.Cos(fRadians);
 		}
 
 		//SetTranslation( f )
 		public void SetTranslation(float x, float y, float z)
 		{
-			m[12] = x;
-			m[13] = y;
-			m[14] = z;
+			m13 = x;
+			m14 = y;
+			m15 = z;
 		}
 
 		//SetTranslation( V )
 		public void SetTranslation(Vector4 pos)
 		{
-			m[12] = pos.x;
-			m[13] = pos.y;
-			m[14] = pos.z;
+			m13 = pos.x;
+			m14 = pos.y;
+			m15 = pos.z;
 		}
 
 		//SetScale( f )
 		public void SetScale(float x, float y, float z)
 		{
-			m[0] = x;
-			m[5] = y;
-			m[10] = z;
+			m1 = x;
+			m6 = y;
+			m11 = z;
 		}
 
 		//SetScale( V )
 		public void SetScale(Vector4 scale)
 		{
-			m[0] = scale.x;
-			m[5] = scale.y;
-			m[10] = scale.z;
+			m1 = scale.x;
+			m6 = scale.y;
+			m11 = scale.z;
 		}
 
 	}
